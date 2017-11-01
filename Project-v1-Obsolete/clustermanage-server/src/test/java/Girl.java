e6fb2667895655f16bd798b0885dce317f9bd30a
bitkylin <bitkylin@163.com>
2017-11-01 15:59:13 +0800

    修改工程目录结构

---
+++ Project-v1-Obsolete/clustermanage-server/src/test/java/Girl.java
@@ -0,0 +1,15 @@
+/**
+ * Created by lmlLocal on 2017/5/25.
+ */
+public class Girl extends Person {
+
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int a) {
+        age = a;
+    }
+}
---
+++ Project-v1-Obsolete/clustermanage-server/src/test/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
+public class Main {
+    public static void main(String[] lml) {
+
+
+    }
+
+    public void run(@NotNull List<String> strings) {
+
+    }
+}
+
