38b23474c266e0e44ef771f53a257e55a13de79a
bitkylin <bitkylin@163.com>
2017-11-01 15:59:13 +0800

    修改工程目录结构

---
+++ Project-v1-Obsolete/clustermanage-server/src/test/java/Person.java
@@ -0,0 +1,11 @@
+/**
+ * Created by lmlLocal on 2017/5/25.
+ */
+public class Person {
+
+    int age = 18;
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ Project-v1-Obsolete/clustermanage-server/src/test/java/cc/bitky/clustermanage/db/KyTest.java
@@ -0,0 +1,17 @@
+package cc.bitky.clustermanage.db;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class KyTest {
+//    @Autowired
+//    private StringRedisTemplate stringRedisTemplate;
+//
+//    @Test
+//    public void contextLoads() {
+//        // 保存字符串
+//        stringRedisTemplate.opsForValue().set("aaa", "111");
+//        Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+//
+//    }
+
+}
