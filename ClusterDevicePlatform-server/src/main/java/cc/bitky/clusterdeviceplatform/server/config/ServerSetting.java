2bbd76238b295d3b74e8656c4464a247a4aade5b
bitkylin <bitkylin@163.com>
2018-05-16 00:27:14 +0800

    调整项目的版本信息

--- ClusterDevicePlatform-server/src/main/java/cc/bitky/clusterdeviceplatform/server/config/ServerSetting.java
+++ ClusterDevicePlatform-server/src/main/java/cc/bitky/clusterdeviceplatform/server/config/ServerSetting.java
@@ -14,7 +14,7 @@ public class ServerSetting {
     /**
      * 项目版本号
      */
-    public static final String VERSION = "1.13.0";
+    public static final String VERSION = "1.15.0";
     /**
      * 本地配置文件名
      */
