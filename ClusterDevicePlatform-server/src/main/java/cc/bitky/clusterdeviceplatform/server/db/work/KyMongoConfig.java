23aa66b24c6c09ed5caecec6266e278bdc7cfe6f
bitkylin <bitkylin@163.com>
2020-10-09 17:41:25 +0800

    mongoDB连接优化升级

--- ClusterDevicePlatform-server/src/main/java/cc/bitky/clusterdeviceplatform/server/db/work/KyMongoConfig.java
+++ ClusterDevicePlatform-server/src/main/java/cc/bitky/clusterdeviceplatform/server/db/work/KyMongoConfig.java
@@ -1,29 +1,24 @@
 package cc.bitky.clusterdeviceplatform.server.db.work;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
+import cc.bitky.clusterdeviceplatform.server.config.DbSetting;
+import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 
-import cc.bitky.clusterdeviceplatform.server.config.DbSetting;
-
+/**
+ * @author limingliang
+ */
 @Configuration
-public class KyMongoConfig extends AbstractMongoConfiguration {
+public class KyMongoConfig {
 
     @Bean
-    public com.mongodb.reactivestreams.client.MongoClient reactiveMongoClient() {
-        if (DbSetting.AUTHENTICATION_STATUS) {
-            return MongoClients.create("mongodb://" + DbSetting.DATABASE_USERNAME + ":" + DbSetting.DATABASE_PASSWORD + "@" + DbSetting.MONGODB_HOST + ":" + DbSetting.MONGODB_PORT + "/" + DbSetting.DATABASE);
+    public MongoClient reactiveMongoClient() {
+        if (DbSetting.databaseAuthenticationStatus) {
+            return MongoClients.create("mongodb://" + DbSetting.databaseUsername + ":" + DbSetting.databasePassword + "@" + DbSetting.mongodbHost + ":" + DbSetting.mongodbPort + "/" + DbSetting.database);
         } else {
-            return MongoClients.create("mongodb://" + DbSetting.MONGODB_HOST + ":" + DbSetting.MONGODB_PORT);
+            return MongoClients.create("mongodb://" + DbSetting.mongodbHost + ":" + DbSetting.mongodbPort);
         }
     }
 
@@ -32,29 +27,7 @@ public class KyMongoConfig extends AbstractMongoConfiguration {
         return new ReactiveMongoTemplate(reactiveMongoClient(), getDatabaseName());
     }
 
-    @Override
-    public MongoClient mongoClient() {
-        if (DbSetting.AUTHENTICATION_STATUS) {
-            return new MongoClient(
-                    new ServerAddress(DbSetting.MONGODB_HOST, DbSetting.MONGODB_PORT),
-                    MongoCredential.createCredential(DbSetting.DATABASE_USERNAME, getDatabaseName(), DbSetting.DATABASE_PASSWORD.toCharArray()),
-                    MongoClientOptions.builder().build()
-            );
-        } else {
-            return new MongoClient(new ServerAddress(DbSetting.MONGODB_HOST, DbSetting.MONGODB_PORT));
-        }
-    }
-
-    @Override
-    protected String getDatabaseName() {
-        return DbSetting.DATABASE;
-    }
-
-    @Override
-    public MappingMongoConverter mappingMongoConverter() throws Exception {
-        MappingMongoConverter mmc = super.mappingMongoConverter();
-        //remove _class
-        mmc.setTypeMapper(new DefaultMongoTypeMapper(null));
-        return mmc;
+    private String getDatabaseName() {
+        return DbSetting.database;
     }
 }
